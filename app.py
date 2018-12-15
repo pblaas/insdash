45731af8b0769870ba38fa19b342b237ae67e98e
Patrick Blaas <patrick@kite4fun.nl>
2018-12-15 11:23:47 +0100

    Update captcha layout

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ import logging
 from flask_caching import Cache
 
 __author__ = "Patrick Blaas <patrick@kite4fun.nl>"
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 if "SERVERIP" not in os.environ:
     os.environ["SERVERIP"] = "83.96.176.30"
