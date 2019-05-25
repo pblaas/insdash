0043e9c9ab0e5c35e0053065e5809462b871b805
Patrick Blaas <patrick@kite4fun.nl>
2019-05-25 09:07:12 +0200

    Bump version  maplist fix

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ import logging
 from flask_caching import Cache
 
 __author__ = "Patrick Blaas <patrick@kite4fun.nl>"
-__version__ = "1.0.4"
+__version__ = "1.0.5"
 
 if "SERVERIP" not in os.environ:
     os.environ["SERVERIP"] = "83.96.176.30"
